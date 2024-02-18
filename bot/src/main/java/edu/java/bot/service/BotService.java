36bc8b20bccef146ee09f0bf58a29938148c82b1
WolfOfBad <mechkov.igor04@gmail.com>
2024-02-18 23:28:45 +0400

    Added menu buttons (for bonus point)

--- bot/src/main/java/edu/java/bot/service/BotService.java
+++ bot/src/main/java/edu/java/bot/service/BotService.java
@@ -2,14 +2,19 @@ package edu.java.bot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import com.pengrad.telegrambot.response.BaseResponse;
 import edu.java.bot.configuration.ApplicationConfigProperties;
 import edu.java.bot.model.command.Command;
 import java.util.List;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BotService implements UpdatesListener, AutoCloseable {
     private final CommandParser parser;
 
@@ -20,7 +25,36 @@ public class BotService implements UpdatesListener, AutoCloseable {
         bot = new TelegramBot(configProperties.telegramToken());
         this.parser = parser;
 
+
+        setCommandsMenu();
+
         bot.setUpdatesListener(this);
+    }
+
+    private void setCommandsMenu() {
+        BotCommand startCommand = new BotCommand("start", "Начать работу с ботом");
+        BotCommand helpCommand = new BotCommand("help", "Список доступных команд");
+        BotCommand trackCommand = new BotCommand("track", "Начать отслеживание ссылки");
+        BotCommand untrackCommand = new BotCommand("untrack", "Прекратить отслеживание ссылки");
+        BotCommand listCommand = new BotCommand("list", "Список отслеживаемых ссылок");
+        BotCommand resetCommand = new BotCommand("reset", "Сбросить все отслеживаемые ссылки");
+
+        SetMyCommands request = new SetMyCommands(
+            startCommand,
+            helpCommand,
+            trackCommand,
+            untrackCommand,
+            listCommand,
+            resetCommand
+        );
+
+        BaseResponse response = bot.execute(request);
+
+        if (response.isOk()) {
+            log.info("Commands menu successfully set");
+        } else {
+            log.info("Commands menu not set:" + response.errorCode());
+        }
     }
 
     @Override
